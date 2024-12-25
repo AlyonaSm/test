69edb986519c36059c6e8eacc6fa1a5f03eedb03
AlyonaSm <haalen32@gmail.com>
2024-12-25 16:10:54 +0300

    commit comment

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-let firstName = 'Alyona'
-let age = '28'
-let city = 'Moscow'
-let hobbies = 'Pilates, reading'
-console.log('hello', firstName)
+const titleProject = 'Оценка стоимости'
+const screensValue = 'Шаблонные, с уникальным дизайном, с анимациями'
+const screenPrice = 12
+const percentage = 10
+const fullPrice = 12332412321n
+const responsive = true
+console.log(typeof responsive)
+
